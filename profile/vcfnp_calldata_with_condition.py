be041622b7a4f042afffd2d51cef96d7ffc2ccdc
Alistair Miles <alimanfoo@googlemail.com>
2013-05-16 16:32:20 +0100

    add profiling scripts

---
+++ profile/vcfnp_calldata_with_condition.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import pstats
+import cProfile
+import timeit
+
+
+sys.path.insert(0, '.')
+import vcfnp
+
+
+def profile():
+    v = vcfnp.variants(sys.argv[1], count=int(sys.argv[2]))
+    c = vcfnp.calldata(sys.argv[1], condition=v['FILTER']['PASS'])
+
+
+prof_fn = 'profile/tmp.prof'
+cmd = 'profile()'
+cProfile.runctx(cmd, globals(), locals(), prof_fn)
+s = pstats.Stats(prof_fn)
+s.strip_dirs().sort_stats('time').print_stats()
+print timeit.repeat(cmd, number=int(sys.argv[3]), repeat=int(sys.argv[4]), setup='from __main__ import profile')
+
+
