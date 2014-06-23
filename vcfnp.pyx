4de53c69f1246472b129387bec131f4ae38b035f
Alistair Miles <alimanfoo@googlemail.com>
2014-06-23 16:35:13 +0100

    increment version

--- vcfnp.pyx
+++ vcfnp.pyx
@@ -14,7 +14,8 @@ Utility functions to extract data from a VCF file and load into a numpy array.
 # 1.10.2
 # 1.11
 # 1.11.1
-__version__ = '1.11.2'
+# 1.11.2
+__version__ = '1.12-SNAPSHOT'
 
 
 import sys
