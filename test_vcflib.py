72594a22100a00707cb21ddc162cf3aaa2871df8
Alistair Miles <alimanfoo@googlemail.com>
2015-01-21 19:11:53 +0000

    test_vcflib passing under PY3

--- test_vcflib.py
+++ test_vcflib.py
@@ -1,10 +1,8 @@
-"""
-Some simple unit tests for the Cython vcflib wrapper.
-
-"""
+from __future__ import print_function, division, absolute_import
 
 
-from vcflib import PyVariantCallFile, TYPE_INTEGER, TYPE_FLOAT, TYPE_STRING, TYPE_BOOL
+from vcflib import PyVariantCallFile, TYPE_INTEGER, TYPE_FLOAT, TYPE_STRING, \
+    TYPE_BOOL
 from nose.tools import eq_
 
 
@@ -15,53 +13,51 @@ def test_len():
 
 def test_metadata():
     vcf = PyVariantCallFile('fixture/sample.vcf')
-    eq_(['NS', 'AN', 'AC', 'DP', 'AF', 'AA', 'DB', 'H2'], vcf.infoIds)
-    eq_(['GT', 'GQ', 'DP', 'HQ'], vcf.formatIds)
-    eq_(['s50', 'q10'], vcf.filterIds)
-    eq_(TYPE_INTEGER, vcf.infoTypes['NS'])
-    eq_(TYPE_FLOAT, vcf.infoTypes['AF'])
-    eq_(TYPE_STRING, vcf.infoTypes['AA'])
-    eq_(TYPE_BOOL, vcf.infoTypes['DB'])
-    eq_(TYPE_STRING, vcf.formatTypes['GT'])
-    eq_(TYPE_INTEGER, vcf.formatTypes['GQ'])
-    eq_(1, vcf.infoCounts['AA'])
-    eq_(2, vcf.formatCounts['HQ'])
-    eq_(['NA00001', 'NA00002', 'NA00003'], vcf.sampleNames)
+    eq_(['NS', 'AN', 'AC', 'DP', 'AF', 'AA', 'DB', 'H2'], vcf.info_ids)
+    eq_(['GT', 'GQ', 'DP', 'HQ'], vcf.format_ids)
+    eq_(['s50', 'q10'], vcf.filter_ids)
+    eq_(TYPE_INTEGER, vcf.info_types['NS'])
+    eq_(TYPE_FLOAT, vcf.info_types['AF'])
+    eq_(TYPE_STRING, vcf.info_types['AA'])
+    eq_(TYPE_BOOL, vcf.info_types['DB'])
+    eq_(TYPE_STRING, vcf.format_types['GT'])
+    eq_(TYPE_INTEGER, vcf.format_types['GQ'])
+    eq_(1, vcf.info_counts['AA'])
+    eq_(2, vcf.format_counts['HQ'])
+    eq_(['NA00001', 'NA00002', 'NA00003'], vcf.sample_names)
 
 
 def test_fixed_fields():
     vcf = PyVariantCallFile('fixture/sample.vcf')
-    v = iter(vcf).next() # first variant
+    v = next(iter(vcf))  # first variant
     eq_('19', v.CHROM)
     eq_(111, v.POS)
     eq_('.', v.ID)
     eq_('A', v.REF)
     eq_(['C'], v.ALT)
     eq_(9.6, v.QUAL)
-    eq_(['.'], v.FILTER) # split in C++
+    eq_(['.'], v.FILTER)  # split in C++
     
     
 def test_info():
     vcf = PyVariantCallFile('fixture/sample.vcf')
-    v = list(iter(vcf))[4] # fifth variant
+    v = list(iter(vcf))[4]  # fifth variant
     expect = {'AA': ['T'], 'NS': ['2'], 'DP': ['10'], 'AF': ['0.333', '0.667']}
     eq_(expect, v.INFO)
 
 
 def test_samples():
     vcf = PyVariantCallFile('fixture/sample.vcf')
-    v = list(iter(vcf))[2] # third variant
+    v = list(iter(vcf))[2]  # third variant
     expect = {'GT': ['0|0'], 'HQ': ['51', '51'], 'GQ': ['48'], 'DP': ['1']}
     eq_(expect, v.samples['NA00001'])
 
 
 def test_region():
     vcf = PyVariantCallFile('fixture/sample.vcf.gz')
-    vcf.setRegion('20')
+    vcf.set_region('20')
     eq_(6, len(vcf))
-    vcf.setRegion('20:1000000-2000000')
+    vcf.set_region('20:1000000-2000000')
     eq_(4, len(vcf))
-    vcf.setRegion('20', 1000000, 2000000)
+    vcf.set_region('20', 1000000, 2000000)
     eq_(4, len(vcf))
-    
-    
--- vcflib.pyx
+++ vcflib.pyx
@@ -1,20 +1,25 @@
-"""
-Cython wrapper for classes defined in Variant.cpp.
-
-Try to keep it simple and stay close to the C++ API.
-
-"""
+from __future__ import print_function, division, absolute_import
 
 
+import sys
 from cython.operator cimport dereference as deref
 from collections import namedtuple
-import numpy as np
-cimport numpy as np
-import time
-import sys
- 
 
-VariantTuple = namedtuple('Variant', ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'samples'])
+
+# PY2/3 compatibility
+PY2 = sys.version_info[0] == 2
+if PY2:
+    text_type = unicode
+    binary_type = str
+else:
+    text_type = str
+    binary_type = bytes
+
+
+VariantTuple = namedtuple(
+    'Variant',
+    ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'samples']
+)
 
 
 # expose constants to Python
@@ -25,10 +30,41 @@ TYPE_STRING = FIELD_STRING
 TYPE_UNKNOWN = FIELD_UNKNOWN
 
 
+def _py(x):
+    if isinstance(x, binary_type):
+        # always pass text to Python
+        y = text_type(x, 'ascii')
+    elif isinstance(x, list):
+        y = [_py(i) for i in x]
+    elif isinstance(x, tuple):
+        y = tuple(_py(i) for i in x)
+    elif isinstance(x, dict):
+        y = {_py(k): _py(v) for (k, v) in x.items()}
+    else:
+        y = x
+    return y
+
+
+def _c(x):
+    if isinstance(x, text_type):
+        # always pass bytes to C
+        y = x.encode('ascii')
+    elif isinstance(x, list):
+        y = [_c(i) for i in x]
+    elif isinstance(x, tuple):
+        y = tuple(_c(i) for i in x)
+    elif isinstance(x, dict):
+        y = {_c(k): _c(v) for (k, v) in x.items()}
+    else:
+        y = x
+    return y
+
+
 cdef class PyVariantCallFile:
 
     def __cinit__(self, filename):
         self.thisptr = new VariantCallFile()
+        filename = _c(filename)
         self.thisptr.open(filename)
 
     def __dealloc__(self):
@@ -45,90 +81,110 @@ cdef class PyVariantCallFile:
     def __iter__(self):
         cdef Variant *var
         cdef vector[string] filters
-        cdef char semicolon = ';'
+        cdef char semicolon = b';'
         var = new Variant(deref(self.thisptr))
         while self.thisptr.getNextVariant(deref(var)):
             # split the filter field here in C++ to avoid having to do it in Python later
             filters = split(var.filter, semicolon)
-            yield VariantTuple(var.sequenceName, 
-                               var.position, 
-                               var.id, 
-                               var.ref, 
-                               var.alt, 
-                               var.quality, 
-                               filters,
-                               var.info,
-                               var.samples)
+            t = _py([
+                var.sequenceName,
+                var.position,
+                var.id,
+                var.ref,
+                var.alt,
+                var.quality,
+                filters,
+                var.info,
+                var.samples
+            ])
+            yield VariantTuple(*t)
         del var
         
-    def setRegion(self, *args):
+    def set_region(self, *args):
         if len(args) == 1:
-            self.thisptr.setRegion(args[0])
+            s = _c(args[0])
+            self.thisptr.setRegion(s)
         elif len(args) == 3:
-            self.thisptr.setRegion(args[0], args[1], args[2])
+            chrom, start, stop = _c(args)
+            start = int(start)
+            stop = int(stop)
+            self.thisptr.setRegion(chrom, start, stop)
         else:
-            raise Exception('either provide a single region string or provide seq, start, end')
+            raise Exception('either provide a single region string '
+                            'or provide chrom, start, stop')
 
-    property infoIds:
+    property info_ids:
         def __get__(self):
-            return self.thisptr.infoIds()
+            l = _py(<list>self.thisptr.infoIds())
+            return l
 
-    property formatIds:
+    property format_ids:
         def __get__(self):
-            return self.thisptr.formatIds()
+            l = _py(<list>self.thisptr.formatIds())
+            return l
 
-    property filterIds:
+    property filter_ids:
         def __get__(self):
-            return self.thisptr.filterIds()
+            l = _py(<list>self.thisptr.filterIds())
+            return l
 
-    property infoTypes:
+    property info_types:
         def __get__(self):
-            return self.thisptr.infoTypes
+            d = _py(<dict>self.thisptr.infoTypes)
+            return d
 
-    property formatTypes:
+    property format_types:
         def __get__(self):
-            return self.thisptr.formatTypes
+            d = _py(<dict>self.thisptr.formatTypes)
+            return d
 
-    property infoCounts:
+    property info_counts:
         def __get__(self):
-            return self.thisptr.infoCounts
+            d = _py(<dict>self.thisptr.infoCounts)
+            return d
 
-    property formatCounts:
+    property format_counts:
         def __get__(self):
-            return self.thisptr.formatCounts
-        
-    property parseSamples:
+            d = _py(<dict>self.thisptr.formatCounts)
+            return d
+
+    property parse_samples:
         def __get__(self):
-            return self.thisptr.parseSamples
+            return <bool>self.thisptr.parseSamples
         def __set__(self, v):
             self.thisptr.parseSamples = v
 
     property header:
         def __get__(self):
-            return self.thisptr.header
-        
-    property fileformat: # [sic] no camel case
+            s = _py(self.thisptr.header)
+            return s
+
+    property file_format: # [sic] no camel case
         def __get__(self):
-            return self.thisptr.fileformat
+            s = _py(self.thisptr.fileformat)
+            return s
         
-    property fileDate:
+    property file_date:
         def __get__(self):
-            return self.thisptr.fileDate
+            s = _py(self.thisptr.fileDate)
+            return s
         
     property source:
         def __get__(self):
-            return self.thisptr.source
+            s = _py(self.thisptr.source)
+            return s
         
     property reference:
         def __get__(self):
-            return self.thisptr.reference
+            s = _py(self.thisptr.reference)
+            return s
         
     property phasing:
         def __get__(self):
-            return self.thisptr.phasing
+            s = _py(self.thisptr.phasing)
+            return s
         
-    property sampleNames:
+    property sample_names:
         def __get__(self):
-            return self.thisptr.sampleNames
-        
-        
+            l = _py(<list>self.thisptr.sampleNames)
+            return l
